c1f8b890a1d988b46c74fb244b3e9fedb9455efd
ACastilloSantiago <santiagorodrigec@gmail.com>
2023-12-04 16:38:32 -0300

    add-form

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
+const $form = document.querySelector("#form");
+
 const handleForm = (event) => {
   event.preventDefault();
   const form = new FormData($form);
   const name = form.get("name");
   const email = form.get("email");
   const message = form.get("message");
-  console.log(name, email, message);
-  fetch("https://formsubmit.co/santiagorodrigec@gmail.com", {
+
+  if (!name) {
+    return;
+  }
+  if (!email) {
+    return;
+  }
+  if (!/^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/.test(email)) {
+    return;
+  }
+  if (!message) {
+    return;
+  }
+  fetch("https://formsubmit.co/ajax/d6099137673011c9e3548cc20169a4e0", {
     method: "POST",
-    body: {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
       name,
       email,
       message,
-    },
+    }),
   });
+  window.alert("Enviado con exito!");
 };
-const $form = document.querySelector("#form");
 $form.addEventListener("submit", handleForm);
-const $submit = document.querySelector("#submit");
-$form.addEventListener("click", handleSubmit);
